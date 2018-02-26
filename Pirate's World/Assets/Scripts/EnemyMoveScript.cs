29bdf0434741fbb82102f87e589f44ed9ab2f724
TobyProsser <tobye.prosser@yahoo.com>
2018-02-25 23:02:17 -0800

    Intial Commit

---
+++ Pirate's World/Assets/Scripts/EnemyMoveScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveScript : MonoBehaviour {
+
+    public Vector3 Destination;
+
+    public float TimeBetween = 5;
+
+    public float speedOfBoat = 10;
+    public GameObject boat;
+
+    public Rigidbody controller;
+    public float heading;
+    public float heading1;
+
+    private float xValMin;
+    private float xValMax;
+    private float zValMin;
+    private float zValMax;
+
+    // Use this for initialization
+    void Awake()
+    {
+        heading = boat.transform.position.x;
+        heading1 = boat.transform.position.z;
+
+        controller = GetComponent<Rigidbody>();
+
+        StartCoroutine(NewHeading());
+    }
+
+    void Update()
+    {
+        boat.transform.LookAt(Destination);
+        //transform.rotation = Quaternion.Lerp(from.rotation, to.rotation, Time.time * speed);
+        float step = speedOfBoat * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, Destination, step);
+    }
+
+    IEnumerator NewHeading()
+    {
+        while (true)
+        {
+            NewHeadingRoutine();
+            print("Distination: " + Destination);
+            yield return new WaitForSeconds(TimeBetween);
+        }
+    }
+
+    void NewHeadingRoutine()
+    {
+        xValMin = .2f;
+        xValMax = 112f;
+        heading = Random.Range(xValMin, xValMax);
+
+        zValMin = .2f;
+        zValMax = 112f;
+        heading1 = Random.Range(zValMin, zValMax);
+
+        float yVal = boat.transform.position.y;
+        Destination = new Vector3(heading, yVal, heading1);
+    }
+}
---
+++ Pirate's World/Assets/Scripts/Float.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour {
+
+    public GameObject Object;
+    private IEnumerator coroutine;
+    private float ObjectPosition;
+    private float ObjectStartPosition;
+    // Use this for initialization
+
+    void Start ()
+    {
+        ObjectStartPosition = Object.transform.position.y; //get starting y value
+        coroutine = WaitAndPrint(2.0f);
+        StartCoroutine(coroutine);
+
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (true)
+        {
+
+            Vector3 temp = new Vector3(-50.0f, 0, 0);
+            Object.transform.position += temp;
+            ObjectPosition = Object.transform.position.y; //get current y value
+            if (ObjectPosition < ObjectStartPosition - 200) //one current value goes X below starting value turn around
+            {
+                Vector3 temp1 = new Vector3(+50.0f, 0, 0);
+                Object.transform.position += temp1;
+            }
+        }
+    }
+}
